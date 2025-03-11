b994312d0f81f995bafaeeb108aa7508629207cc
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2025-03-11 13:03:23 +0530

    typo mistake

--- Let's make a Cake/index.js
+++ Let's make a Cake/index.js
@@ -5,7 +5,7 @@ function setTheme(theme) {
         root.style.setProperty('--text-color', '#ECF2FF');
         root.style.setProperty('--title-color', '#ECF2FF');
     } else if (theme === 'dark') {
-        root.style.setProperty('--bh-colro, #2B283A');
+        root.style.setProperty('--bg-color', '#2B283A');
         root.style.setProperty('--text-color', '#ECF2FF');
         root.style.setProperty('--title-color', '#D5D4D8');
     }
