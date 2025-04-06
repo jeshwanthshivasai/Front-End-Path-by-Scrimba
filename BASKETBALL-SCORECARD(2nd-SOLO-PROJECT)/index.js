eedd4d440b33d2e2da1c3806ccfd4f1bdbffdeb4
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2025-04-06 06:46:27 +0530

    initial commit (just done with text inside card and bg

---
+++ BASKETBALL-SCORECARD(2nd-SOLO-PROJECT)/index.js
@@ -0,0 +1,9 @@
+let container = document.getElementById('container');
+let scorehome = document.getElementById('score-home');
+let scoreguest = document.getElementById('score-guest');
+
+let counthome = 0;
+let countguest = 0;
+
+scorehome.textContent = counthome;
+scoreguest.textContent = countguest;
