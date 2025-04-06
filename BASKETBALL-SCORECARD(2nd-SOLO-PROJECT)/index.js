69d8f3da28b8127c8296e06be4216898a99e0ae8
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2025-04-06 16:45:19 +0530

    javascript done

--- BASKETBALL-SCORECARD(2nd-SOLO-PROJECT)/index.js
+++ BASKETBALL-SCORECARD(2nd-SOLO-PROJECT)/index.js
@@ -7,3 +7,33 @@ let countguest = 0;
 
 scorehome.textContent = counthome;
 scoreguest.textContent = countguest;
+
+function hone() {
+    counthome += 1;
+    scorehome.textContent = counthome;
+}
+
+function htwo() {
+    counthome += 2;
+    scorehome.textContent = counthome;
+}
+
+function hthree() {
+    counthome += 3;
+    scorehome.textContent = counthome;
+}
+
+function gone() {
+    countguest += 1;
+    scoreguest.textContent = countguest;
+}
+
+function gtwo() {
+    countguest += 2;
+    scoreguest.textContent = countguest;
+}
+
+function gthree() {
+    countguest += 3;
+    scoreguest.textContent = countguest;
+}
