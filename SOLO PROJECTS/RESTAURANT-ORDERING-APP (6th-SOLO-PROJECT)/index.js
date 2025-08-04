5b6dd4385926e7e3572807b472a22f60aa7da661
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-08-04 19:37:06 +0530

    did basic stuff to kick off (rendered the data tho)

---
+++ SOLO PROJECTS/RESTAURANT-ORDERING-APP (6th-SOLO-PROJECT)/index.js
@@ -0,0 +1,21 @@
+import menuArray from "./data.js";
+// console.log('hello')
+
+const menuItems = (menuArray) => {
+    return menuArray.map((menu) => {
+        return `
+            <section class="card">
+                <span class="emoji">${menu.emoji}</span>
+                <div class="info">
+                    <h2>${menu.name}</h2>
+                    <p>${menu.ingredients}</p>
+                    <h3>${menu.price}</h3>
+                </div>
+                <i class="fa-solid fa-circle-plus fa-2xl"></i>
+            </section> 
+        `
+    }).join('')
+} 
+
+document.getElementById('content-menu').innerHTML = menuItems(menuArray)
+// console.log(menuItems(menuArray));
