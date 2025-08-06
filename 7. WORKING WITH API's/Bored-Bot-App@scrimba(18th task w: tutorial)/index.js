c6c9edfdf92fca9f8cd8a15a96ae4f9813ecb1e8
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-08-06 20:41:56 +0530

    completed bored-bot-app

--- 7. WORKING WITH API's/Bored-Bot-App@scrimba(18th task w: tutorial)/index.js
+++ 7. WORKING WITH API's/Bored-Bot-App@scrimba(18th task w: tutorial)/index.js
@@ -7,6 +7,22 @@ Challenge:
   you'd like!
 */
 
+const Get = document.getElementById('get-btn')
+
+Get.addEventListener('click', function() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+        document.getElementById("activity").textContent = data.activity
+        document.getElementById("container").classList.toggle('change')
+    })
+})
+
+
+
 // fetch("https://apis.scrimba.com/bored/api/activity")
 //     .then(response => response.json())
 //     .then(data => {
