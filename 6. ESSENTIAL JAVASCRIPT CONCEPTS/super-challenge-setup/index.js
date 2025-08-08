133c80e7a7bec144e4319db8c47ece8766de2417
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-08-08 19:01:57 +0530

    super challenge from essential js is done!!

--- 6. ESSENTIAL JAVASCRIPT CONCEPTS/super-challenge-setup/index.js
+++ 6. ESSENTIAL JAVASCRIPT CONCEPTS/super-challenge-setup/index.js
@@ -1,10 +1,36 @@
-function getPropertyHtml() {
+import propertyForSaleArr from "./properties/propertyForSaleArr.js"
+import placeholderPropertyObj from "./properties/placeholderPropertyObj.js"
+
+function getPropertyHtml(properties = [placeholderPropertyObj]) {
+    return properties.map((property) => {
+        const {image, propertyLocation, priceGBP, comment, roomsM2} = property
+        console.log(property)
+        return `
+            <section class="card">
+                <img src="images/${image}">
+                <div class="card-right">
+                    <h2>${propertyLocation}</h2>
+                    <h3>${priceGBP}</h3>
+                    <p>${comment}</p>
+                    <h3>${roomsM2.reduce((total, initial)=>total+initial)} m&sup2;</h3>
+                </div>
+            </section> 
+        `
+    }).join('')
+}
+
+/***** Modify 👇 by adding an argument to the function call ONLY. *****/
+document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
+
 /*
 SUPER CHALLENGE 💪
 
-Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties' folder). Each card should have an image, a property location, a price, a comment and the TOTAL property size in square metres (each object has an array with the size in square metres of the individual rooms).
+Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties' folder). 
+Each card should have an image, a property location, a price, a comment and the TOTAL property size in square metres 
+(each object has an array with the size in square metres of the individual rooms).
 
-If no array of properties is passed to getPropertyHtml, the placeholder property stored in placeholderPropertyObj (in the 'properties' folder) should be rendered instead.
+If no array of properties is passed to getPropertyHtml, the placeholder property stored in placeholderPropertyObj 
+(in the 'properties' folder) should be rendered instead.
 
 This is the JS I want you to use to complete this challenge 👇
 - import/export
@@ -27,7 +53,3 @@ This is the HTML template 👇. Replace everything in UPPERCASE with property da
     </div>
 </section> 
 */
-}
-
-/***** Modify 👇 by adding an argument to the function call ONLY. *****/
-document.getElementById('container').innerHTML = getPropertyHtml()
--- 6. ESSENTIAL JAVASCRIPT CONCEPTS/super-challenge-setup/properties/propertyForSaleArr.js
+++ 6. ESSENTIAL JAVASCRIPT CONCEPTS/super-challenge-setup/properties/propertyForSaleArr.js
@@ -30,3 +30,4 @@ const propertyForSaleArr = [
 ]
 
 
+export default propertyForSaleArr
