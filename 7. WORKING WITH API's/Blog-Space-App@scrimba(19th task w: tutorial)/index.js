e6e225dfe0d675f22e33b4b3915e3ddbdac738aa
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-08-11 12:45:04 +0530

    started blog space project

---
+++ 7. WORKING WITH API's/Blog-Space-App@scrimba(19th task w: tutorial)/index.js
@@ -0,0 +1,18 @@
+/**
+ Challenge:
+ 
+ GET a list of blog posts from the JSON Placeholder API.
+ 
+ BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ Endpoint: /posts
+ 
+ Since there's so many posts, let's limit the array to just 5 items.
+ You can use the `.slice()` array method to just grab the first 5 objects
+ from the data array that comes back from the API
+ 
+ Log the 5 items to the console
+ */
+
+fetch('https://apis.scrimba.com/jsonplaceholder/posts')
+.then(res => res.json())
+.then(data => console.log(data.slice(1, 6)))
