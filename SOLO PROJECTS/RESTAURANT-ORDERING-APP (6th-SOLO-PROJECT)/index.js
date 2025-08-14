8ad029561ee458399e6c55cc03cb8749c530ed04
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-08-14 16:05:27 +0530

    goddamn pwd and restaurant changes

--- SOLO PROJECTS/RESTAURANT-ORDERING-APP (6th-SOLO-PROJECT)/index.js
+++ SOLO PROJECTS/RESTAURANT-ORDERING-APP (6th-SOLO-PROJECT)/index.js
@@ -4,18 +4,22 @@ import menuArray from "./data.js";
 const menuItems = (menuArray) => {
     return menuArray.map((menu) => {
         return `
-            <section class="card">
-                <span class="emoji">${menu.emoji}</span>
-                <div class="info">
-                    <h2>${menu.name}</h2>
-                    <p>${menu.ingredients}</p>
-                    <h3>${menu.price}</h3>
+            <section id="card">
+                <div id="item">
+                    <span id="emoji">${menu.emoji}</span>
+                    <div id="info">
+                        <h2>${menu.name}</h2>
+                        <p>${menu.ingredients}</p>
+                        <h3>$${menu.price}</h3>
+                    </div>
                 </div>
-                <i class="fa-solid fa-circle-plus fa-2xl"></i>
-            </section> 
+                <button id="add-btn" type="button">+</button>
+            </section>
+            <hr id="line">
         `
     }).join('')
 } 
 
 document.getElementById('menu-container').innerHTML = menuItems(menuArray)
 // console.log(menuItems(menuArray));
+// <i id="add-btn" class="fa-solid fa-circle-plus fa-2xl"></i>
