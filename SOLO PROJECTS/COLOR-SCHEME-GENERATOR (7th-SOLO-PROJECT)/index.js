90a7ed98fb754fe3779ae9a12f40f0920e7cff68
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-08-15 21:09:16 +0530

    day 1 update help with nishi

--- SOLO PROJECTS/COLOR-SCHEME-GENERATOR (7th-SOLO-PROJECT)/index.js
+++ SOLO PROJECTS/COLOR-SCHEME-GENERATOR (7th-SOLO-PROJECT)/index.js
@@ -1,25 +1,42 @@
-let colors
+let colorScheme
+let colorPickerEl = document.getElementById('color-picker')
 let colorDropdown = document.getElementById('color-dropdown')
 let colorSchemes = document.getElementById('color-schemes')
 let colorHexCodes = document.getElementById('color-hex-codes')
+let getBtn = document.getElementById('get-btn')
 
+window.addEventListener('DOMContentLoaded', getColorSchemes)
+
+//to take input from colorpicker and generate hex code in console
+let colorPicker = colorPickerEl.addEventListener('change', () => {
+    const selectedColor = colorPickerEl.valueß
+})
+
+//fetches color schemes from the color api
 const getColorSchemes = () => {
     fetch('https://www.thecolorapi.com/scheme?hex=0047AB')
     .then(res => res.json())
     .then(colors => {
-        console.log(colors)
-        console.log(colors._links.schemes)
-
-
-
+        //fetching color schemes and storing the keys of the object
+        colorScheme = Object.keys(colors._links.schemes)
+        getColorScheme(colorScheme)
+        
     })
 }
 
-document.getElementById(get-btn).addEventListener('submit', getColorSchemes)
+const getColorScheme = (colorScheme) => {
+    colorScheme.map(color => {
+        colorDropdown.innerHTML += `
+            <option value="${color}">${color}</option>
+        `
+    })
+}
+
+console.log(colorDropdown.value)
+getBtn.addEventListener('click', getColorSchemes)
+console.log(colorDropdown)
 
 
-    colors = Object.keys(colors._links.schemes)
-colorDropdown.innerHTML
-    return colors.map(color => `
-        <option>${color}</option>
-        `)
+
+    // colorDropdown.innerHTML = ''
+    // console.log(colorDropdown)
