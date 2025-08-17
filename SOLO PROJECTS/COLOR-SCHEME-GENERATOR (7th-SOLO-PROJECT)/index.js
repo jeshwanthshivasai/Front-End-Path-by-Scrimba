1d4061b63c7e7e480482bf488189baace4ce205c
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-08-17 22:20:57 +0530

    completed color schemes project (with help of AI)

--- SOLO PROJECTS/COLOR-SCHEME-GENERATOR (7th-SOLO-PROJECT)/index.js
+++ SOLO PROJECTS/COLOR-SCHEME-GENERATOR (7th-SOLO-PROJECT)/index.js
@@ -1,35 +1,46 @@
-let colorScheme
 let colorPickerEl = document.getElementById('color-picker')
-let colorDropdown = document.getElementById('color-dropdown')
-let colorSchemes = document.getElementById('color-schemes')
-let colorHexCodes = document.getElementById('color-hex-codes')
-let getBtn = document.getElementById('get-btn')
+let colorDropdownEl = document.getElementById('color-dropdown')
+let colorSchemesEl = document.getElementById('color-schemes')
+let colorHexCodesEl = document.getElementById('color-hex-codes')
+let getBtnEl = document.getElementById('get-btn')
 
-window.addEventListener('DOMContentLoaded', getColorSchemes)
-
-//to take input from colorpicker and generate hex code in console
-let colorPicker = colorPickerEl.addEventListener('change', () => {
-    const selectedColor = colorPickerEl.value
-})
-
-//fetches color schemes from the color api
-const getColorSchemes = () => {
-    fetch('https://www.thecolorapi.com/scheme?hex=0047AB')
+//fetch the color modes on page load by attaching a even listener to the window and
+//adding an event which loads immediately after the html is finished parsing
+window.addEventListener('DOMContentLoaded', () => {
+    fetch(`https://www.thecolorapi.com/scheme?hex=000000&mode=monochrome&count=1`)
     .then(res => res.json())
     .then(colors => {
-        //fetching color schemes and storing the keys of the object
-        colorScheme = Object.keys(colors._links.schemes)
-        getColorScheme(colorScheme)
-        
+        const colorSchemes = Object.keys(colors._links.schemes)
+        // console.log(colorSchemes)
+        for(const colorScheme of colorSchemes) {
+            // console.log(colorScheme)
+            colorDropdownEl.innerHTML += `
+                <option value="${colorScheme}">${colorScheme}</option>
+            `
+        }
     })
-}
+})
 
-const getColorScheme = (colorScheme) => {
-    colorScheme.map(color => {
-        colorDropdown.innerHTML += `
-            <option value="${color}">${color}</option>
-        `
+
+getBtnEl.addEventListener('click', () => {
+    const selectedColor = colorPickerEl.value.slice(1)
+    const selectedScheme = colorDropdownEl.value
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedScheme}&count=5`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        colorSchemesEl.innerHTML = ""
+        colorHexCodesEl.innerHTML = ""
+
+        for (const color of data.colors) {
+            colorSchemesEl.innerHTML += `
+                <div class="color" style="background-color:${color.hex.value}"></div>
+            `
+            // console.log(color.hex.value)
+            colorHexCodesEl.innerHTML += `
+                <p class="hex">${color.hex.value}</p>
+            `
+        }
     })
-}
-
-getBtn.addEventListener('click', getColorSchemes)
+})
